3d155a0b9eac6613f5a3eb483218abe5b0d1edbf
evelyn-owl <alisa.golysheva.s@gmail.com>
2024-07-24 20:15:33 +0300

    Create 6-owl-house.py

---
+++ 6-owl-house.py
@@ -0,0 +1,42 @@
+# TODO: не питай Камілу про школу та інше 
+# пишу у змінні
+kids = input("Do you have kids? ")
+if kids == "yes":
+  print("Hi Camilla!")
+else:
+  eyes = input("Do you have 2 eyes? ")
+  if eyes == "no":
+    print("Hi Ugly Bosha!")
+  else:
+    gender = input("Are you a girl or a boy? ")
+    if gender == "boy":
+      print("Bye-bye Belos!")
+    else:
+      hero = input("Are you a witch or a human? ")
+      if hero == "human":
+        print("Go Luz!!!")
+      else:
+        school = input("Do you study at school? ")
+        if school == "yes":
+          print("Hi Glove!")
+        else:
+          print("Hi Ida!")
+
+        
+# перевіряю героїв
+# Ida, Emity, Bosha, Imperor Belos
+# Luz, Camilla
+# if hero == "witch" and school == "no":
+#   if gender == "girl":
+#     print("Hi Ida!")
+#   else:
+#     print("Bye-bye Belos!")
+# elif hero == "witch" and school == "yes":
+#   if eyes == "yes":
+#     print("Hi Glove!")
+#   else:
+#     print("Hi Ugly Bosha!")
+# #elif kids == "yes":
+# #  print("Hi Camilla!")
+# else:
+#    print("Go Luz!!!")
