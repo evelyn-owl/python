7fc9e64699e8a131ecdfd794963cddef858f9b58
evelyn-owl <alisa.golysheva.s@gmail.com>
2024-07-25 13:32:28 +0300

    Create 7-owl-house-fake-fan-finder.py

---
+++ 7-owl-house-fake-fan-finder.py
@@ -0,0 +1,26 @@
+
+print("🕵️‍♀️ Fake Fan Finder 👀")
+print("---------------------")
+
+movie = input("What is your favourite show? ")
+if movie != "owl house":
+  print("Get out of here!")
+else:
+  hero = input("Name me any of the characters? ")
+  if hero  == "Bosha":
+    hair = input("What color is her hair? ")
+    if hair != "pink" and hair != "dark pink":
+      print("See! FAKE fan! 😡😡😡😡😡")
+    else:
+      print("Ok, you are a fan! 🥳🥳🥳🥳🥳🥳🥳🥳🥳")
+  
+  elif hero == "Emity":
+    alias = input("What is her alias? ")
+    if alias != "glove":
+      print("See! FAKE fan! 😡😡😡😡😡")
+    else:
+      print("Ok, you are a fan! 🥳🥳🥳🥳🥳🥳🥳🥳🥳")
+    
+  else:
+    print("See you soon!!!")
+
