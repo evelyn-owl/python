812d29308d9537c4425c32a6452ad95d45f00b33
evelyn-owl <alisa.golysheva.s@gmail.com>
2024-07-27 18:47:33 +0300

    Create 9-jokes.py

---
+++ 9-jokes.py
@@ -0,0 +1,7 @@
+jokes = int(input("How many jokes do you know? "))
+if jokes < 6:
+  print(" you are sad koala")
+if jokes > 6 and jokes < 100:
+  print("you are Master of Jokes ")
+if jokes > 100:
+  print("Don't be liar Henry!!!")
