9573d96d58877257367406ae023f4e2157265829
evelyn-owl <alisa.golysheva.s@gmail.com>
2024-07-31 13:41:39 +0300

    Update 9-jokes.py

--- 9-jokes.py
+++ 9-jokes.py
@@ -1,7 +1,8 @@
+name = input("What's your name? ")
 jokes = int(input("How many jokes do you know? "))
 if jokes < 6:
   print(" you are sad koala")
 if jokes > 6 and jokes < 100:
   print("you are Master of Jokes ")
 if jokes > 100:
-  print("Don't be liar Henry!!!")
+  print(f"Don't be liar {name}!!!")
