cab42d86b847658ee3c003f4cd211d1b18099034
evelyn-owl <alisa.golysheva.s@gmail.com>
2024-08-02 17:49:21 +0300

    Create 15-meow.py

---
+++ 15-meow/15-meow.py
@@ -0,0 +1,28 @@
+while exit != "yes":
+  animal = input("choose your animal: dog, cat ")
+  if animal == "cat":
+    print(f"A {animal} goes meow")
+  
+  elif animal == "dog":
+    print(f"A {animal} goes bark")
+    
+  exit = input("Do you want to exit?: ")
+
+# counter = 0
+# while counter < 10:
+#  print(counter)
+#  counter += 1
+
+# exit = ""
+# while exit != "yes":
+#   print("🎭👗👢♊♀️AT")
+#   exit = input("Exit?: ")
+
+# counter = 0
+# while counter < 25:
+#   print(counter)
+#   counter+= 1 
+# counter = 0
+# while counter < 12:
+#   print(counter)
+#   counter += 1
