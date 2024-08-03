8f265359d9c6bbd09b178526ff9f3524e9e2df78
evelyn-owl <alisa.golysheva.s@gmail.com>
2024-08-03 18:20:56 +0300

    Update 16.py

--- 16.py
+++ 16.py
@@ -2,19 +2,29 @@
 print("""Fill in the blank lyrics!
 (Type in the blank lyrics and see if you are as cool as me.)
 """)
+
 attempts = 0
+line = "Almighty crown of _______ "
+key = "victory"
+
 while True:
+  if attempts == 4:
+    key = "crown"
+    line = "Almighty _____ of victory"
+  if attempts == 7:
+    key = "ground"
+    line = "Crash into the ______ but at least the nightmare ends"
+
   attempts += 1
-  word = input("Almighty crown of _______ ")
-  if word != "victory":
+  
+  word = input(line)
+  if word != key:
     print("Nope, try again.")
     print()
   else:
     print(f"Well done! It only took you {attempts} attempts.")
     break
     
-
-
 # while True:
 #   color = input("Enter a color: ")
 #   if color == "red":
