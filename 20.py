757c801b905f76f69374ebad6d22780fe5c4306e
evelyn-owl <alisa.golysheva.s@gmail.com>
2024-08-07 18:07:11 +0300

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,9 @@
+print("List Generator")
+print()
+start = int(input("Start at: "))
+end = int(input("End before: "))
+inc = int(input("Increment: "))
+print()
+for i in range (start, end, inc):
+  print(i)
+  
